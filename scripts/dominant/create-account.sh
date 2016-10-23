442b137273ba4697f3e22c562418a0945d4630ae
Andrey Mayorov <a.mayorov@rbkmoney.com>
2016-10-23 18:18:33 +0300

    HG-63: Fill proper account ids in the base fixture

---
+++ scripts/dominant/create-account.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+CURRENCY=${1}
+shift 1
+
+[ -z "${CURRENCY}" ] && { echo "No currency code specified"; exit -1; }
+
+woorl $* \
+    -s damsel/proto/accounter.thrift \
+    http://shumway:8022/accounter \
+    Accounter CreateAccount "{\"currency_sym_code\":\"${CURRENCY}\"}"
