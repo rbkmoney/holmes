410acfc28a30dfb552a803e62dcfe3f559d18b95
Andrew Mayorov <encube.ul@gmail.com>
2018-04-09 20:18:05 +0300

    Put a couple of scripts on a shelf for later use (#40)
    Drop some legacy along the way.

---
+++ scripts/repair-invoice.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USAGE=$(cat <<EOF
+Usage: ${SCRIPTNAME} invoice-id invoice-changes
+  Repairs an invoice and stuffs it with the user-provided list of invoice
+  changes.
+  invoice-id           Invoice ID (string)
+  invoice-changes      Invoice changes (json array)
+
+More information:
+  https://github.com/rbkmoney/damsel
+EOF
+)
+
+function usage {
+    echo "${USAGE}"
+    exit 127
+}
+
+[ -f woorlrc ] && source woorlrc
+
+INVOICE="${1}"
+[ -z "${INVOICE}" ] && usage
+INVOICE_CHANGES="${2}"
+[ -z "${INVOICE_CHANGES}" ] && usage
+
+USERINFO='{"id":"woorl","type":{"service_user":{}}}'
+
+${WOORL:-woorl} \
+    -s damsel/proto/payment_processing.thrift \
+    "http://${HELLGATE:-hellgate}:8022/v1/processing/invoicing" \
+    Invoicing Repair "${USERINFO}" "\"${INVOICE}\"" "${INVOICE_CHANGES}"
---
+++ scripts/submit-posting-plan.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+USAGE=$(cat <<EOF
+Usage: ${SCRIPTNAME} plan-id batch
+  Prepares and commits a plan made up of the single posting batch provided by
+  the user.
+  plan-id     Posting plan ID (string)
+  batch       Posting batch (json object, see [2])
+
+More information:
+  [1]: https://github.com/rbkmoney/damsel
+  [2]: https://github.com/rbkmoney/damsel/blob/b0806eb1/proto/accounter.thrift#L67
+EOF
+)
+
+function usage {
+    echo "${USAGE}"
+    exit 127
+}
+
+[ -f woorlrc ] && source woorlrc
+
+PLANID="${1}"
+[ -z "${PLANID}" ] && usage
+BATCH="${2}"
+[ -z "${BATCH}" ]  && usage
+
+ACCOUNTER="http://${SHUMWAY:-shumway}:8022/accounter"
+
+${WOORL:-woorl} -s damsel/proto/accounter.thrift \
+    ${ACCOUNTER} Accounter Hold \
+    "{\"id\": \"${PLANID}\", \"batch\": ${BATCH}}" && \
+${WOORL:-woorl} -s damsel/proto/accounter.thrift \
+    ${ACCOUNTER}  Accounter CommitPlan \
+    "{\"id\": \"${PLANID}\", \"batch_list\": [${BATCH}]}"
