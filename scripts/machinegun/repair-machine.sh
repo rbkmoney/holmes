0b62e973e45be8b87163716d1a67a86f1feb74b5
Andrew Mayorov <a.mayorov@rbkmoney.com>
2019-01-23 15:58:18 +0300

    Respect woorlrc more often + put sample to the repo root

--- scripts/machinegun/repair-machine.sh
+++ scripts/machinegun/repair-machine.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+[ -f woorlrc ] && source woorlrc
+
 MACHINE_NS="\"${1}\""
 MACHINE_ID="{\"id\":\"${2}\"}"
 shift 2
 
 woorl $* \
     -s damsel/proto/state_processing.thrift \
-    http://${MACHINEGUN}:${THRIFT_PORT}/v1/automaton \
+    http://${MACHINEGUN:-machinegun}:8022/v1/automaton \
     Automaton Repair ${MACHINE_NS} ${MACHINE_ID} \
         '{"content_type":"base64","content": "g2o="}'
