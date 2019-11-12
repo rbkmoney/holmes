6a32cc50bc573d2d314c89f1d8ed0f654c14975e
Andrew Mayorov <a.mayorov@rbkmoney.com>
2019-11-12 18:27:57 +0300

    Bring back precompiled scripts to the image (#68)

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -16,7 +16,7 @@ ENV THRIFT_PORT=8022 \
 COPY ./damsel/proto    /opt/holmes/damsel/proto
 COPY ./cds_proto/proto /opt/holmes/cds_proto/proto
 COPY ./scripts         /opt/holmes/scripts
-#COPY ./lib/scripts     /opt/holmes/scripts
+COPY ./lib/scripts     /opt/holmes/scripts
 CMD epmd
 # A bit of magic below to get a proper branch name
 # even when the HEAD is detached (Hey Jenkins!
