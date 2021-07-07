55a12b0efbd10f071fdbbc0e08f2790b8c1d57f8
0x42 <bpozdnyakov@gmail.com>
2021-07-07 14:26:44 +0300

    add getopt

--- scripts/limiter/configurate_limiter.sh
+++ scripts/limiter/configurate_limiter.sh
@@ -11,63 +11,55 @@ trap "rm -rf ${LIMITER_PROTO}/proto/proto" EXIT
 
 source "${CWD}/../lib/logging"
 
-function usage {
-  echo -e "Create configuration usage in limit counting."
-  echo
-  echo -e "Usage: $(em ${SCRIPTNAME} id started_at name description --set-body BODY --set-behaviour BEHAVIOUR)"
-  echo -e "  $(em id)              Limit config ID (string)"
-  echo -e "  $(em started_at)      Timestamp (RFC3339 timestamp) Example: \"2021-07-06T01:02:03Z\""
-  echo -e "  $(em name)            Limit configuration name.(string)"
-  echo -e "  $(em description)     Limit description (string)"
-  echo -e "  OPTIONAL: "
-  echo -e "  $(em --set-body)      Set limit body type amount or currency. BODY:"
-  echo -e "     $(em -amount)         Body type amount"
-  echo -e "     $(em -currency)       Body type currency(ISO 4217)"
-  echo -e "  $(em --set-behaviour) Limiter behaviour when process payment refund. BEHAVIOUR:"
-  echo -e "     $(em -subtraction)"
-  echo -e "     $(em -addition)"
+USAGE=$(cat <<EOF
 
-  echo
-  echo -e "More information:"
-  echo -e "  https://github.com/rbkmoney/limiter-proto/blob/master/proto/configurator.thrift"
-  exit 127
+  Create configuration usage in limit counting.
+  Usage: ${SCRIPTNAME}  [--set-body-currency CUR | --set-body-amount] --subtraction id started_at name description
+    id                        Limit config ID (string)
+    started_at                Timestamp (RFC3339 timestamp) Example: \"2021-07-06T01:02:03Z\"
+    name                      Limit configuration name (string)
+    description               Limit description (string)
+
+    OPTIONAL:
+
+    --set-body-currency       Set limit body type currency. Body type currency(ISO 4217)
+    --set-body-amount         Set limit body type amount
+    --subtraction             Limiter behaviour when process payment refund. After refund limit will decrease on refund amount.
+
+  More information:
+    https://github.com/rbkmoney/limiter-proto/blob/master/proto/configurator.thrift
+EOF
+)
+
+function usage {
+    echo "${USAGE}"
+    exit 127
 }
+
+TEMP=$(getopt -o "" --long help,set-body-currency:,set-body-amount,subtraction -n "${SCRIPTNAME}" -- "$@")
+[ $? != 0 ] && usage
+
+eval set -- "${TEMP}"
+
+while true; do
+  case "$1" in
+    --help                    )  usage ;;
+    --set-body-currency       )  BODY_TYPE=",\"body_type\":{\"cash\": {\"currency\": \"${2}\"}}" ; shift 2 ;;
+    --set-body-amount         )  BODY_TYPE=",\"body_type\":{\"amount\": {}}"                     ; shift 2 ;;
+    --subtraction             )  BEHAVIOUR=", \"op_behaviour\": {\"invoice_payment_refund\": {\"subtraction\": {}}}" ; shift 1 ;;
+    --                        ) shift 1 ; break ;;
+    *                         ) break ;;
+  esac
+done
 
 ID="${1}"
+[ -z "${ID}" ] && usage
 STARTED_AT="${2}"
+[ -z "${STARTED_AT}" ] && usage
 NAME="${3}"
+[ -z "${NAME}" ] && usage
 DESCRIPTION="${4}"
-BODY_TYPE=""
-BEHAVIOUR=""
-
-behaviour () {
-    case $1 in
-      -subtraction ) OP="{\"subtraction\": {}}" ;;
-      -addition    ) OP="{\"addition\": {}}"    ;;
-      *            ) usage                      ;;
-    esac
-
-    BEHAVIOUR=", \"op_behaviour\": {\"invoice_payment_refund\": ${OP}}"
-}
-
-if [ "${5}" = '--set-body' ]; then
-  case "$6" in
-    -amount   ) BODY_TYPE=",\"body_type\":{\"amount\": {}}" ;;
-    -currency ) BODY_TYPE=",\"body_type\":{\"cash\": {\"currency\": \"${7}\"}}" ;;
-    *         ) usage ;;
-  esac
-
-  if [ "${6}" = "-amount" ] && [ "$7" = '--set-behaviour' ]; then
-    behaviour "${8}"
-  fi
-  if [ "${6}" = "-currency" ] && [ "$8" = '--set-behaviour' ]; then
-    behaviour "${9}"
-  fi
-fi
-
-if [ "${5}" = '--set-behaviour' ]; then
-  behaviour "${6}"
-fi
+[ -z "${DESCRIPTION}" ] && usage
 
 
 JSON=$(cat <<END
